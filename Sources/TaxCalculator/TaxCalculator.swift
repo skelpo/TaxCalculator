5441bdde3950341c4dab10db37614f5cd8992018
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-05 14:11:25 -0600

    Documented TaxCalculator protocol

--- Sources/TaxCalculator/TaxCalculator.swift
+++ Sources/TaxCalculator/TaxCalculator.swift
@@ -2,8 +2,17 @@ import Foundation
 import Service
 import NIO
 
+/// A type that can get the tax amount from the total of an online order.
 public protocol TaxCalculator: Service {
+    
+    /// The type that can be passed into the calculator to get the tax value.
     associatedtype Input
     
+    /// Calculates a tax amount from an instance of the `Input` type.
+    ///
+    /// - Parameter input: The value to get the tax amount from.
+    ///
+    /// - Returns: The tax amount, wrapped in an `EventLoopFuture`. This is done in
+    ///   case the calculator needs to make an API request, such as TaxJar.
     func tax(for input: Input) -> EventLoopFuture<Decimal>
 }
